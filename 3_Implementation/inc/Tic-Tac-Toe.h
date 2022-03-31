77dc490189029ecc6647b7022844175c396c032d
AVINASH-P-1912 <102513343+AVINASH-P-1912@users.noreply.github.com>
2022-03-31 15:00:25 +0530

    Update Tic-Tac-Toe.h

--- 3_Implementation/inc/Tic-Tac-Toe.h
+++ 3_Implementation/inc/Tic-Tac-Toe.h
@@ -1,1 +1,9 @@
+#include<stdio.h>
+#include<string.h>
+char x,o;
+char a[9]={'1','2','3','4','5','6','7','8','9'};
+char k[50],l[50];
+void leader_board();
+void check_rules();
+int check_for_win();
 
