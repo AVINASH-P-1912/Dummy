9c61dc6d963c2538a7f986851976658c2b4f0a78
AVINASH-P-1912 <102513343+AVINASH-P-1912@users.noreply.github.com>
2022-03-31 15:05:00 +0530

    Create decision_making.c

---
+++ 3_Implementation/functions/decision_making.c
@@ -0,0 +1,15 @@
+int decision_making()
+{
+    char dec;
+        asci:
+        printf("\n\nPlayer1 %s choose the X or 0:",k);
+        dec=getchar();
+        scanf("%c",&dec);
+        {
+            if(dec=='X' || dec=='x')
+            {
+                x='X';
+                o='0';
+            }
+        }
+}
