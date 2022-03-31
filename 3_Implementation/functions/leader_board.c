e8b9def840e27104202926e06fcbb2c5ea663ff0
AVINASH-P-1912 <102513343+AVINASH-P-1912@users.noreply.github.com>
2022-03-31 15:03:34 +0530

    Create leader_board.c

---
+++ 3_Implementation/functions/leader_board.c
@@ -0,0 +1,17 @@
+void leader_board()
+{
+    int i;
+    system("cls");
+    printf("\tTic-Tac-Toe\n\n");
+        printf("\n\n");
+        printf("%s:- (%c)\n%s:-  (%c)\n\n\n",k,x,l,o);
+        printf("  %c |  %c | %c\n",a[0],a[1],a[2]);
+        printf("    |    |    \n");
+        printf("----|----|----\n");
+        printf("    |    |    \n");
+        printf("  %c |  %c | %c\n",a[3],a[4],a[5]);
+        printf("    |    |    \n");
+        printf("----|----|----\n");
+        printf("  %c |  %c | %c\n",a[6],a[7],a[8]);
+        printf("    |    |    \n");
+    }
