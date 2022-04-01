4971020b84f770cc49eb95fbd2b402a1703ed527
AVINASH-P-1912 <102513343+AVINASH-P-1912@users.noreply.github.com>
2022-04-01 22:16:56 +0530

    Update check_rules.c

--- 3_Implementation/functions/check_rules.c
+++ 3_Implementation/functions/check_rules.c
@@ -1,11 +1,12 @@
+#include"Tic-Tac-Toe.h"
 void check_rules()
 {
     char link;
     printf("\tTic-Tac-Toe\n\n");
-    printf("Welcome to the most played 2D game and a sort of fun using X and O\n\n");
+    printf("Welcome to Tic-Tac-Toe !!\n\n");
     printf("Rules:\n");
-    printf("\n1:Each player will be entering the number to put respective X or O in the desired position\n");
-    printf("\n2:Player who gets a combination of 3 same characters either diagonal or horizontally or vertically will be declared \nas the winner");
+    printf("\n1:Players will be entering any number between 1-9  to put respective X or O in the desired position\n");
+    printf("\n2:Player who gets a combination of 3 same characters either diagonally or horizontally or vertically will be declared \nas the winner");
     printf("\n\nEnjoy the game! Be a Winner!\n\n");
     printf("For more clarifications press Y else type any other character:- ");
     scanf("%c",&link);
